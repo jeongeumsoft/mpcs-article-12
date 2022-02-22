f0c4a920b638eb1ef235d01538221db35165a565
hwajun Lee <hj-lee@crestec.co.kr>
2022-02-22 14:29:35 +0900

    seed 업데이트

--- database/factories/ArticleFactory.php
+++ database/factories/ArticleFactory.php
@@ -18,7 +18,7 @@ $factory->define(Article::class, function (Faker $faker) {
     $title = $faker->sentence();
     $dateBetween = $faker->dateTimeBetween('-30 days', '+30 days', 'ASIA/SEOUL');
     $date = $faker->dateTimeThisMonth;
-
+    $content = $faker->paragraphs(5, true);
     // 이미지 랜덤 생성 FAKER
     // $ratio = ['400', '500', '600', '700', '800', '900'];
     // $aspectRatio = Arr::random($ratio) . "x" . Arr::random($ratio);
@@ -30,7 +30,8 @@ $factory->define(Article::class, function (Faker $faker) {
     return [
         'title' => $title,
         'summary' => $faker->paragraph(),
-        'content' => $faker->paragraph(),
+        'content' => $content,
+        'html' => $content,
         //'thumbnail' => Bootstrap5::generateThumb('articles', $imageName),
         'view_count' => mt_rand(0, 1000000),
         'released_at' => function () use ($dateBetween) {
