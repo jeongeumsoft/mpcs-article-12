804c9c7ccbd576f708b23f72469ae945bd8c0e44
hwajun Lee <ezrock@naver.com>
2024-12-27 13:45:41 +0900

    route redirect 설정

--- config/mpcsarticle.php
+++ config/mpcsarticle.php
@@ -3,7 +3,7 @@
 return [
 
     // 공개된 API 사용 여부
-    'enable_open_api' => false,
+    'enable_open_api' => env('ARTICLE_OPEN_API_ENABLE', false),
 
     'category_max_depth' => env('ARTICLE_CATEGORY_MAX_DEPTH', 4),
 
